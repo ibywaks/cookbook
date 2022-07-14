159a41a4deb9d228bc9c188d7ecf9a3c99d78022
Matt Matheson <matt@trunk.io>
2022-07-14 09:44:29 -0700

    Install and set up Buildkite Test Analytics

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,14 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
+  // setupFilesAfterEnv: ['./tests/bootstrap.ts']
   verbose: true,
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  reporters: ["default", "jest-junit"],
-  // setupFilesAfterEnv: ['./tests/bootstrap.ts']
+  reporters: ["default", "buildkite-test-collector/jest/reporter"],
+
+  // Enable column + line capture for Test Analytics
+  testLocationInResults: true,
 };
 export default config;
