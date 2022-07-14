97e0adc83714366d6a6851f8f1d179a0b7f2a0e4
Matt Matheson <matt@trunk.io>
2022-07-13 17:19:08 -0700

    add junit reporting

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config.InitialOptions = {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
+  reporters: ["default", "jest-junit"],
   // setupFilesAfterEnv: ['./tests/bootstrap.ts']
 };
 export default config;
